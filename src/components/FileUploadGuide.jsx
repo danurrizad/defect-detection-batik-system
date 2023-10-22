16a03f6318840f9f0a7947d84072002f70966d0c
danurrizad <danurrizad242@gmail.com>
2023-10-23 05:48:23 +0700

    add: guide for uploading file

---
+++ src/components/FileUploadGuide.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const FileUploadGuide = () => {
+  return (
+    <div>
+        <div>
+            <h1>Requirement To Upload File CSV</h1>
+        </div>
+    </div>
+  )
+}
+
+export default FileUploadGuide
