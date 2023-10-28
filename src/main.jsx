f7bc886e8eb9858ff5b32703bb4f1f27cf594f7e
danurrizad <danurrizad242@gmail.com>
2023-10-28 21:20:21 +0700

    add: forecast context provider

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,15 @@ import './index.css'
 import './tailwind.css'
 import { CsvDataProvider } from './components/CsvDataContext'
 import { UserProvider } from './components/UserContext.jsx'
+import { ForecastValueProvider } from './components/context/ForecastValueContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <UserProvider>
       <CsvDataProvider>
-        <App />
+        <ForecastValueProvider>
+          <App />
+        </ForecastValueProvider>
       </CsvDataProvider>
     </UserProvider>
   </React.StrictMode>,
