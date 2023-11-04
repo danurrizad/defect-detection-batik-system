ee4a453d355bd64d355fbb3d558a357c5446cced
danurrizad <danurrizad242@gmail.com>
2023-11-05 01:00:42 +0700

    style: change opacity loading

--- src/components/LoadingPage.jsx
+++ src/components/LoadingPage.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const LoadingPage = ({text}) => {
   return (
-    <div className=' min-h-screen bg-slate-200 bg-opacity-60 fixed w-screen top-0 left-0 z-10'>
+    <div className=' min-h-screen bg-slate-200 bg-opacity-90 fixed w-screen top-0 left-0 z-10'>
         <div className="flex flex-col justify-center items-center h-screen ">
           <div className="border-t-8 border-primary1 border-solid rounded-full w-16 h-16 animate-spin"></div>
-          <h1 className='animate-pulse'>{text}</h1>
+          <h1 className='animate-pulse font-bold'>{text}</h1>
         </div>
     </div>
   );
