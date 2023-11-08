c3bbb6841f24915cdcebeddea73b55470112b3d4
danurrizad <danurrizad242@gmail.com>
2023-11-09 00:37:56 +0700

    fix: import

--- src/pages/Detection.jsx
+++ src/pages/Detection.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import Header from '../components/Header'
-import WebcamYOLOComponent from '../components/WebcamScanner5'
+import WebcamYOLOComponent from '../components/WebcamScanner'
 import Footer from '../components/Footer'
 
 const Detection = () => {
